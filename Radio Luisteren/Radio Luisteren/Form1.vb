1c0d92540b9a93ae0c8f24dec66b4278cc2fba02
BuzzPrograms <joel.weber@live.nl>
2014-07-29 13:14:16 +0200

    Radio Luisteren

---
+++ Radio Luisteren/Radio Luisteren/Form1.vb
@@ -0,0 +1,76 @@
+﻿Imports System.Net
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        AxWindowsMediaPlayer1.URL = "http://icecast.omroep.nl/radio1-bb-mp3"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        AxWindowsMediaPlayer1.URL = "http://icecast.omroep.nl/radio2-bb-mp3"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        AxWindowsMediaPlayer1.URL = "http://icecast.omroep.nl/3fm-bb-mp3"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        AxWindowsMediaPlayer1.URL = "http://icecast.omroep.nl/radio4-bb-mp3"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        AxWindowsMediaPlayer1.URL = "http://icecast.omroep.nl/radio5-bb-mp3"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        AxWindowsMediaPlayer1.URL = "http://icecast.omroep.nl/radio6-bb-mp3"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        AxWindowsMediaPlayer1.URL = "http://icecast.omroep.nl/funx-bb-mp3"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        AxWindowsMediaPlayer1.URL = "http://stream.100p.nl/100pctnl.mp3"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        AxWindowsMediaPlayer1.URL = "http://82.201.100.9:8000/radio538.m3u"
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        AxWindowsMediaPlayer1.URL = "http://vip2.str.reasonnet.com/qmusic.mp3.96.m3u"
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        AxWindowsMediaPlayer1.URL = "http://8543.live.streamtheworld.com/VERONICACMP3"
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        AxWindowsMediaPlayer1.URL = "http://8623.live.streamtheworld.com:80/SKYRADIOAAC_SC"
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        AxWindowsMediaPlayer1.URL = "http://82.201.100.10:8000/SLAMFM_MP3_HQ"
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim web As New WebClient 'To be able to download the content from the latest version file you have stored.
+        Dim LatestVersion As String = web.DownloadString("https://www.dropbox.com/s/wd8qf5k4p2o9yn8/version.txt?dl=1") 'To download the Lastest Version from a specified URL.
+        Dim CurrentApp As String = My.Application.Info.Version.ToString 'Gets the applications current version
+
+
+        If CurrentApp < LatestVersion Then 'If the applications current version is less than the Latest version Then it will update, otherwise just do nothing or w/e you want.
+
+            If MessageBox.Show("Er is een nieuwe update!" & vbNewLine & "Wil je updaten?", "Auto Updater", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = Windows.Forms.DialogResult.Yes Then 'Message box dialog asking the user if they wish to update or not.
+                My.Computer.Network.DownloadFile("https://www.dropbox.com/s/41s57u17144q6u9/Radio%20Luisteren.exe?dl=1", Application.StartupPath & "\Updated\Auto Updater.exe") 'If they choose Yes, it will download the latest version exe
+                MsgBox("The update is gedownload!" & vbNewLine & "De applicatie zal nu sluiten") 'telling the user the app will close
+                My.Computer.FileSystem.DeleteFile(Application.StartupPath & "\Updated\Auto Updater.exe")
+                End 'exits application
+            Else
+                'If they choose No
+            End If
+
+        Else
+
+        End If
+    End Sub
+End Class
---
+++ Radio Luisteren/Radio Luisteren/Update.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Update
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Update))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 54)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(176, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "controleer voor een update"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 12)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(176, 23)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(194, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(10, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "."
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(75, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(10, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "."
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(194, 28)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(38, 32)
+        Me.WebBrowser1.TabIndex = 4
+        Me.WebBrowser1.Visible = False
+        '
+        'Update
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(247, 90)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Update"
+        Me.ShowInTaskbar = False
+        Me.Text = "Update"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+End Class
